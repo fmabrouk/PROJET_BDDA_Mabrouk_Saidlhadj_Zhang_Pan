2ac60d4f47accb7fb9d5785b7e4334ac48aef3a2
fmabrouk <mabroukfayez1@gmail.com>
2022-09-16 11:58:11 +0200

    TP01

---
+++ main.sh
@@ -0,0 +1,4 @@
+
+#!/bin/bash
+javac -d ./CODE/IDE_PROJET_Mabrouk_Saidlhadj_Zhang_Pan/bin  ./CODE/IDE_PROJET_Mabrouk_Saidlhadj_Zhang_Pan/src/Main.java
+java  -classpath ./CODE/IDE_PROJET_Mabrouk_Saidlhadj_Zhang_Pan/bin:. Main
