3cd9b5019a5a6a0fe28508788a65783ddef2b0b0
Mohand <82048076+MohandSL@users.noreply.github.com>
2022-12-01 15:58:39 +0100

    Update HeaderPage.java

--- CODE/IDE_PROJET_Mabrouk_Saidlhadj_Zhang_Pan/src/up/mi/jgm/TP05/HeaderPage.java
+++ CODE/IDE_PROJET_Mabrouk_Saidlhadj_Zhang_Pan/src/up/mi/jgm/TP05/HeaderPage.java
@@ -1,13 +1,14 @@
 package up.mi.jgm.TP05;
 import java.nio.ByteBuffer;
 
-import up.mi.jgm.TP02.DBParams; 
+import up.mi.jgm.TP02.DBParams;
+import up.mi.jgm.TP02.PageId; 
 
 public class HeaderPage {
 	
 	private int mInitPosition = 0;
 	private ByteBuffer mByteBuffer = null;
-	private int nombreDataPage = 0;
+
 	
 	
 	public HeaderPage(ByteBuffer buff) {
@@ -16,7 +17,14 @@ public class HeaderPage {
 	}
 	
 	public void setNombreDataPage() {
-		nombreDataPage++;
+		int nbPages = mByteBuffer.getInt(0);
+		nbPages++;
+		mByteBuffer.putInt(0, nbPages);
+	}
+	
+	public int getNombreDataPage() {
+		int nbPages = mByteBuffer.getInt(0);
+		return nbPages;
 	}
 	
 	
